545ab508eb2f6c21850e1c2e978e853566abd349
Kylee Tilley <kyleetilley@gmail.com>
2018-12-13 22:11:53 -0600

    Refactor

--- src/index.js
+++ src/index.js
@@ -2,23 +2,16 @@ import "@babel/polyfill";
 import { navigate, Router } from "@reach/router";
 import React from "react";
 import ReactDOM from "react-dom";
-import styled from "styled-components";
 
 import SearchPage from "./pages/SearchPage";
 import CreateTopicPage from "./pages/CreateTopicPage";
 
-const Style = styled.div`
-  font-style: bold;
-`;
-
 const App = () => {
   return (
-    <Style>
-      <Router>
-        <CreateTopicPage path="/" onSubmit={topic => navigate(`/${topic}`)} />
-        <SearchPage path="/:topic" />
-      </Router>
-    </Style>
+    <Router>
+      <CreateTopicPage path="/" onSubmit={topic => navigate(`/${topic}`)} />
+      <SearchPage path="/:topic" />
+    </Router>
   );
 };
 
